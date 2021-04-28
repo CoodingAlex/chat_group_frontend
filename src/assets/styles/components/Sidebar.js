d531a7026bdeaad1754453305933857c19d07f27
Alejandro Ibarra <coodingalex@gmail.com>
2021-04-27 18:53:42 -0600

    :construction: [PROGRESS] Add user functionality

--- src/assets/styles/components/Sidebar.js
+++ src/assets/styles/components/Sidebar.js
@@ -1,47 +1,49 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const SidebarContainer = styled.div`
-  color: #bdbdbd;
-  background-color: #120f13;
-  font-family: 'Noto Sans', sans-serif;
-  h3 {
-    color: #e0e0e0;
-  }
-`
+    color: #bdbdbd;
+    background-color: #120f13;
+    font-family: 'Noto Sans', sans-serif;
+    h3 {
+        color: #e0e0e0;
+    }
+`;
 
 export const ChannelsContainer = styled.div`
-  width: 80%;
-  padding: 10px;
-`
+    width: 80%;
+    padding: 10px;
+`;
 export const DescriptionContainer = styled.div`
-  margin: 15px 0;
-  h3 {
-    font-weight: 600;
-    margin-bottom: 10px;
-  }
-`
+    margin: 15px 0;
+    h3 {
+        font-weight: 600;
+        margin-bottom: 10px;
+    }
+`;
 export const MembersContainer = styled.div`
-  h3 {
-    font-weight: 600;
-  }
-`
+    h3 {
+        font-weight: 600;
+    }
+`;
 export const MemberCard = styled.div`
-  display: flex;
-`
+    display: flex;
+`;
 
 export const ChannelHeader = styled.div`
-  display: flex;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25) h3 {
-    margin-left: 10px;
-  }
-`
+    display: flex;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 10px 10px;
+    h3 {
+        margin-left: 10px;
+    }
+`;
 
 export const AllChannelsHeader = styled.div`
-  display: flex;
-  padding: 10px 10px;
-  justify-content: space-between;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`
+    display: flex;
+    padding: 10px 10px;
+    justify-content: space-between;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
 export const Channel = styled.div`
-  margin-bottom: 5px;
-`
+    margin-bottom: 5px;
+`;
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,116 +1,130 @@
-import React, { useState, useEffect } from 'react'
-import socketIoClient from 'socket.io-client'
+import React, { useState, useEffect } from 'react';
+import socketIoClient from 'socket.io-client';
+import axion from 'axios';
+import axios from 'axios';
 
-const ENDPOINT = 'http://localhost:8080'
+const ENDPOINT = 'http://localhost:8080';
 
 const useInitialState = () => {
-  const [currentChat, setCurrentChat] = useState('welcome')
-  const [messages, setMessages] = useState([])
-  const [chats, setChats] = useState([])
-  const [availableRooms, setAvailableRooms] = useState([])
-  const [state, setState] = useState({
-    filteredMessages: [],
-  })
-  const [socket, setSocket] = useState()
+    const [currentChat, setCurrentChat] = useState('welcome');
+    const [messages, setMessages] = useState([]);
+    const [chats, setChats] = useState([]);
+    const [availableRooms, setAvailableRooms] = useState([]);
+    const [state, setState] = useState({
+        filteredMessages: [],
+    });
+    const [socket, setSocket] = useState();
 
-  useEffect(() => {
-    try {
-      const socket = socketIoClient(ENDPOINT)
-      setSocket(socket)
+    useEffect(() => {
+        try {
+            axios.post(
+                'http://localhost:8080/auth/register',
+                { name: 'Alex', photo: '' },
+                {
+                    withCredentials: true
+                }
+            );
+            const socket = socketIoClient(ENDPOINT);
+            setSocket(socket);
 
-      // This happens when you recibe a new message
-      socket.on('message', (data) => {
-          console.log(data)
-        const newMessage = {
-          chat: data.room,
-          message: data.message,
-          name: data.name,
-          photo: data.photo,
-          timestamp: data.timeStamp
-        }
-        setMessages((prev) => [...prev, newMessage])
-      })
+            // This happens when you recibe a new message
+            socket.on('message', (data) => {
+                console.log(data);
+                const newMessage = {
+                    chat: data.room,
+                    message: data.message,
+                    name: data.name,
+                    photo: data.photo,
+                    timestamp: data.timeStamp,
+                };
+                setMessages((prev) => [...prev, newMessage]);
+            });
 
-      socket.on('newRoom', (data) => {
-        const newChat = {
-          name: data.room,
-          users: data.users,
-        }
-        console.log('new room')
-          setAvailableRooms((prev) => [...prev, newChat])
-      })
-      // This happens when you join to a new chat, or someone join to a chat
-      // that you are already in
-      socket.on('joinedRoom', (data) => {
-        const newChat = {
-          name: data.room,
-          users: data.users,
-        }
+            socket.on('newRoom', (data) => {
+                const newChat = {
+                    name: data.room,
+                    users: data.users,
+                };
+                console.log('new room');
+                setAvailableRooms((prev) => [...prev, newChat]);
+            });
+            // This happens when you join to a new chat, or someone join to a chat
+            // that you are already in
+            socket.on('joinedRoom', (data) => {
+                const newChat = {
+                    name: data.room,
+                    users: data.users,
+                };
 
-        //check if chat already exists
-        setChats((prev) => {
-          const exists = prev.filter((chat) => chat.name === newChat.name)
-          if (exists.length === 0) {
-            return [...prev, newChat]
-          }
-          const newData = prev.map(item => {
-              if(item.name === exists[0].name){
-                  item.users = data.users
-                  return item
-              }
-              return item
-          })
-          return [...newData]
-        })
-      })
-      //TODO: implement the error handling
-    } catch (err) {}
-  }, [])
+                //check if chat already exists
+                setChats((prev) => {
+                    const exists = prev.filter(
+                        (chat) => chat.name === newChat.name
+                    );
+                    if (exists.length === 0) {
+                        return [...prev, newChat];
+                    }
+                    const newData = prev.map((item) => {
+                        if (item.name === exists[0].name) {
+                            item.users = data.users;
+                            return item;
+                        }
+                        return item;
+                    });
+                    return [...newData];
+                });
+            });
+            //TODO: implement the error handling
+        } catch (err) {}
+    }, []);
 
-  // This effect update the filtered messages (the messages that should be in the screen)
-  // when the state current chat change or when you recibe a new message
-  useEffect(() => {
-    const filteredMessages = filterMessages(currentChat)
+    // This effect update the filtered messages (the messages that should be in the screen)
+    // when the state current chat change or when you recibe a new message
+    useEffect(() => {
+        const filteredMessages = filterMessages(currentChat);
 
-    setState({
-      ...state,
-      filteredMessages,
-    })
-  }, [currentChat, messages])
+        setState({
+            ...state,
+            filteredMessages,
+        });
+    }, [currentChat, messages]);
 
-  // Filter the messages by the current chat
-  const filterMessages = (chat) => {
-    const filteredMessages = messages.filter((message) => message.chat === chat)
-    return filteredMessages
-  }
+    // Filter the messages by the current chat
+    const filterMessages = (chat) => {
+        const filteredMessages = messages.filter(
+            (message) => message.chat === chat
+        );
+        return filteredMessages;
+    };
 
-  const joinChat = (chat) => {
-    socket?.emit('joinRoom', { room: chat })
-    setAvailableRooms(prev => {
-      return prev.filter(item => item.name !== chat)
-    })
-    setCurrentChat(chat)
-  }
+    const joinChat = (chat) => {
+        socket?.emit('joinRoom', { room: chat });
+        setAvailableRooms((prev) => {
+            return prev.filter((item) => item.name !== chat);
+        });
+        setCurrentChat(chat);
+    };
 
-  const getCurrentChatUsers = () => {
-    const users = chats.filter(item => item.name === currentChat)[0].users
-    return users
-  }
+    const getCurrentChatUsers = () => {
+        const users = chats.filter((item) => item.name === currentChat)[0]
+            .users;
+        return users;
+    };
 
-  const sendMessage = (chat, message = 'aaa') => {
-    socket.emit('message', { room: chat, message })
-  }
-  return {
-    state,
-    setCurrentChat,
-    currentChat,
-    joinChat,
-    sendMessage,
-    messages,
-    chats,
-    availableRooms,
-    getCurrentChatUsers
-  }
-}
+    const sendMessage = (chat, message = 'aaa') => {
+        socket.emit('message', { room: chat, message });
+    };
+    return {
+        state,
+        setCurrentChat,
+        currentChat,
+        joinChat,
+        sendMessage,
+        messages,
+        chats,
+        availableRooms,
+        getCurrentChatUsers,
+    };
+};
 
-export default useInitialState
+export default useInitialState;
